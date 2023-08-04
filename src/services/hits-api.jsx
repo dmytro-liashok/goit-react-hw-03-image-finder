4bd171b9c8967ec4cf25a5126319c7514412a9c7
lyashodm <lyashokdima1@gmail.com>
2023-08-04 13:46:55 +0300

    first

--- src/services/hits-api.jsx
+++ src/services/hits-api.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 async function fetchHits(searchValue, page) {
   try {
     const respons = await axios.get(
-      `https://pixabay.com/api/?&q=${searchValue}&page=${page}&key=10288621-f6b00deda1a1cc64bb1ecec3b&image_type=photo&orientation=horizontal&per_page=50`
+      `https://pixabay.com/api/?&q=${searchValue}&page=${page}&key=10288621-f6b00deda1a1cc64bb1ecec3b&image_type=photo&orientation=horizontal&per_page=12`
     );
     return respons.data;
   } catch (error) {
